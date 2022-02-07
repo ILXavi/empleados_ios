8bad201c529036aaf78664d49e0dff12ce1fdeef
Javier Rodriguez Ardila <javierodriguezar@gmail.com>
2022-02-07 17:10:03 +0100

    Initial Commit

---
+++ Empleados/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Empleados
+//
+//  Created by Javier Eduardo Rodriguez Ardila on 7/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
